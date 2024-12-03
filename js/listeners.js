b94292228215bf1e60187fd822ba4f920583a13a
Kevin Oh <koh705@mtroyal.ca>
2024-12-02 23:07:15 -0700

    oist.innerHTML = '';     list.innerHTML = ''; Process Creation/management added

--- js/listeners.js
+++ js/listeners.js
@@ -1,5 +1,67 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    
+    $("#createProcessBtn").addEventListener("click", () => {
+        createProcess();
+    })
 })
+
+/*
+*   createProcess - Using user input values create a process prototype
+*                   instance
+*               
+*   Details: All id's are gathered from the form submitted by the user in index.html
+*/
+function createProcess() {
+    let process_list = [];
+    if(localStorage.getItem(storage_key)) {
+        process_list = JSON.parse(localStorage.getItem(storage_key));
+    }
+    let new_process = new Process;
+    new_process.name = $("#processName").value;
+    
+    //the new id is equal to the length of the process list before addition
+    new_process.id = process_list.length; 
+
+    //all new processes should be "New"? check with axyl
+    new_process.state = "New";
+
+    new_process.priority = $("#priorityInput").value;
+    new_process.running_chance = $("#runningChanceInput").value / 100;
+    new_process.is_io = $("#isIOInput").value;
+    new_process.required_execution_time = $("#burstTimeInput").value;
+
+    appendProcessList(new_process);
+}
+
+
+
+/*
+*   appendProcessList - Adds a passed process to the process list, updating
+*                       the front and backend.
+*
+*   Details: #editProcessList refers to the html ul element which displays to
+*            the user
+*/
+function appendProcessList(process) {
+    const process_list = $("#editProcessList");
+    //adding to the html
+    let name = document.createElement("li");
+    name.className = "bg-white p-2 rounded shadow";
+    name.textContent = process.name;
+    let id = document.createElement("strong");
+    id.className = "ml-4";
+    id.textContent = "P" + process.id;
+    name.appendChild(id);
+    
+    process_list.appendChild(name);
+
+    //adding to the locally stored array
+    if(localStorage.getItem(storage_key)) {
+        let process_list = JSON.parse(localStorage.getItem(storage_key));
+        process_list.push(process);
+        localStorage.setItem(storage_key, JSON.stringify(process_list));
+    }
+}
+
+
 
--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 function $(selector) {return document.querySelector(selector)}
 function $$(selector) {return document.querySelectorAll(selector)}
-
 const simulation = new Simulator();
 simulation.setSpeed(0.5); // set a default speed
-function run_simulator(){
-    let process_list = [];
-    process_list.push(new Process(0, "systemd",  "New", 0, true, 0.1, 20));
-    process_list.push(new Process(1, "kevin",  "New", 1, true, 0.75, 2));
-    process_list.push(new Process(2, "test1",  "New", 2, true, 0.6, 5));
-    process_list.push(new Process(3, "test2",  "New", 3, true, 0.5, 10));
+
+const storage_key = "processes"; //the key for locally stored processes
+
+
+//Delete the comments if you're okay with how I changed it
+function run_simulator(process_list){
+    //let process_list = [];
+    //process_list.push(new Process(0, "systemd",  "New", 0, true, 0.1, 20));
+    //process_list.push(new Process(1, "kevin",  "New", 1, true, 0.75, 2));
+    //process_list.push(new Process(2, "test1",  "New", 2, true, 0.6, 5));
+    //process_list.push(new Process(3, "test2",  "New", 3, true, 0.5, 10));
     let scheduling_algorithm = $("#scheduler").value;
     simulation.initialize(process_list, scheduling_algorithm);
     simulation.run();
@@ -21,9 +25,25 @@ function updateSliderValue(value) {
   
 
 document.addEventListener("DOMContentLoaded", () => {
+    let process_list = [];
+    if(!(localStorage.getItem(storage_key))) {
+        //The 4 pushes below are test values - delete locally stored array "processes" to make them added.
+        process_list.push(new Process(0, "systemd",  "New", 0, true, 0.1, 20));
+        process_list.push(new Process(1, "kevin",  "New", 1, true, 0.75, 2));
+        process_list.push(new Process(2, "test1",  "New", 2, true, 0.6, 5));
+        process_list.push(new Process(3, "test2",  "New", 3, true, 0.5, 10));
+        
+        localStorage.setItem(storage_key, JSON.stringify(process_list));
+    } else {
+        process_list = JSON.parse(localStorage.getItem(storage_key));
+    }
+    popProcessList("processList");
+    console.dir(process_list)
+
     $("#editProcessBtn").addEventListener("click", () => {
         $("#mainScreen").classList.toggle("hidden");
         $("#pcbScreen").classList.toggle("hidden");
+        popProcessList("editProcessList");
     })
     $("#showResultBtn").addEventListener("click", () => {
         if($("#resultScreen").classList.contains("hidden")) {
@@ -37,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
             $("#simulatorScreen").classList.toggle("hidden");
             $("#resultScreen").classList.toggle("hidden");
         }
-        run_simulator();
+        run_simulator(process_list);
     })
     $("#slider").addEventListener("change", () => {
         updateSliderValue($("#slider").value);
@@ -45,5 +65,51 @@ document.addEventListener("DOMContentLoaded", () => {
     $("#returnMainBtn").addEventListener("click", () => {
         $("#pcbScreen").classList.toggle("hidden");
         $("#mainScreen").classList.toggle("hidden");
+        popProcessList("processList");
     })
+    $("#clearProcessBtn").addEventListener("click", () => {
+        clearProcessList();
+        popProcessList("editProcessList");
+    })
+
+    
 })
+
+/*
+*   popProcessList - Populates the list of processes based on locally stored data
+*
+*   list_id        - the id of the html ul element to populate.
+*
+*   Details: No changes to the actual data is made, only changes the websites html
+*
+*/
+function popProcessList(list_id) {
+    const list = $(`#${list_id}`);
+    list.innerHTML = '';
+    if(localStorage.getItem(storage_key)) {
+        let process_list = JSON.parse(localStorage.getItem(storage_key));
+        process_list.forEach((process) => {
+            let name = document.createElement("li");
+            name.className = "bg-white p-2 rounded shadow";
+            name.textContent = process.name;
+            list.appendChild(name);
+            let id = document.createElement("strong");
+            id.className = "ml-4";
+            id.textContent = "P" + process.id;
+            name.appendChild(id);
+        })
+    }
+
+}
+
+/*
+*   clearProcessList - Deletes all locally stored process data
+*
+*/
+function clearProcessList() {
+    if(localStorage.getItem(storage_key)) {
+        let process_list = [];
+        localStorage.setItem(storage_key, JSON.stringify(process_list));
+    }
+
+}

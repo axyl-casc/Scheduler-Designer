21d19a5b8f78f743b9a0fb74bd73cc648501a404
Kevinmru737 <78282516+Kevinmru737@users.noreply.github.com>
2024-12-06 17:21:24 -0700

    oh

--- js/listeners.js
+++ js/listeners.js
@@ -33,8 +33,29 @@ document.addEventListener("DOMContentLoaded", () => {
             process_list.push(new Process(1, "Process 1",  "None", 1, true, 0.9, 5, 0));
             process_list.push(new Process(2, "Process 2",  "None", 2, true, 0.9, 8, 0));
             process_list.push(new Process(3, "Process 3",  "None", 3, true, 0.9, 10, 0));
+        }else if(setSelection == "minOverHead"){
+            process_list.push(new Process(0, "systemd",  "None", 0, true, 1, 200, 0));
+            process_list.push(new Process(1, "Process 1",  "None", 1, true, 1, 200, 0));
+            process_list.push(new Process(2, "Process 2",  "None", 2, true, 1, 200, 0));
+            process_list.push(new Process(3, "Process 3",  "None", 3, true, 1, 200, 0));
+            process_list.push(new Process(4, "Process 4",  "None", 1, true, 1, 200, 0));
+            process_list.push(new Process(5, "Process 5",  "None", 2, true, 1, 200, 0));
+            process_list.push(new Process(6, "Process 6",  "None", 3, true, 1, 200, 0));
+            process_list.push(new Process(7, "Process 7",  "None", 1, true, 1, 200, 0));
+            process_list.push(new Process(8, "Process 8",  "None", 2, true, 1, 200, 0));
+            process_list.push(new Process(9, "Process 9",  "None", 3, true, 1, 200, 0));
+        }else if(setSelection == "normOverHead"){
+            process_list.push(new Process(0, "systemd",  "None", 0, true, 0.9, 200, 0));
+            process_list.push(new Process(1, "Process 1",  "None", 1, true, 0.9, 200, 0));
+            process_list.push(new Process(2, "Process 2",  "None", 2, true, 0.9, 200, 0));
+            process_list.push(new Process(3, "Process 3",  "None", 3, true, 0.9, 200, 0));
+            process_list.push(new Process(4, "Process 4",  "None", 1, true, 0.9, 200, 0));
+            process_list.push(new Process(5, "Process 5",  "None", 2, true, 0.9, 200, 0));
+            process_list.push(new Process(6, "Process 6",  "None", 3, true, 0.9, 200, 0));
+            process_list.push(new Process(7, "Process 7",  "None", 1, true, 0.9, 200, 0));
+            process_list.push(new Process(8, "Process 8",  "None", 2, true, 0.9, 200, 0));
+            process_list.push(new Process(9, "Process 9",  "None", 3, true, 0.9, 200, 0));
         }
-        
         localStorage.setItem(storage_key, JSON.stringify(process_list));
         popProcessList("processList");
         $("#presetData").value = "default";
